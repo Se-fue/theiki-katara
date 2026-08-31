382d422507d2421deb6ad5b7526076bf6e8ad435
Se-fue <sefue245@gmail.com>
2026-08-31 15:11:30 -0700

    Hola
    Juego

---
+++ Characters/State.gd
@@ -0,0 +1,30 @@
+extends Node
+class_name State;
+
+signal stateChange (newState: State);
+
+enum state{
+	Walk,
+	Idle,
+	Run,
+	Jump,
+	Fall,
+	Atack
+}
+
+
+var mira
+	
+var currentCharacter : CharacterBody2D;
+
+func enter() -> void:
+	pass;
+
+func update_process(delta: float) -> void:
+	pass;
+	
+func update_physic_process(delta: float) -> void:
+	pass;
+	
+func exit() -> void:
+	pass;
